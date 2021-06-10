a60d4b75e7599c94922490e8a384a3bd82bf7098
Duncan Mackenzie <duncanma@microsoft.com>
2021-06-10 16:23:08 -0700

    Fixing comment detection in SQL

--- src/languages/sql.js
+++ src/languages/sql.js
@@ -155,13 +155,11 @@ function(hljs) {
           },
           hljs.C_NUMBER_MODE,
           hljs.C_BLOCK_COMMENT_MODE,
-          COMMENT_MODE,
-          hljs.HASH_COMMENT_MODE
+          COMMENT_MODE
         ]
       },
       hljs.C_BLOCK_COMMENT_MODE,
-      COMMENT_MODE,
-      hljs.HASH_COMMENT_MODE
+      COMMENT_MODE
     ]
   };
 }
